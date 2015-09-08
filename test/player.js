120b938e23b77777e85f41171a7a104efe18d7cb
margie <ly.margarett@gmail.com>
2015-09-08 10:00:31 -0600

    change key bindings to integers from strings in player.js test

--- test/player.js
+++ test/player.js
@@ -4,10 +4,10 @@ const Player = require('../lib/player');
 const Game   = require('../lib/game');
 
 const keys = {
-  left:  "37",
-  up:    "38",
-  right: "39",
-  down:  "40"
+  left:  37,
+  up:    38,
+  right: 39,
+  down:  40
 };
 
 describe('player', function() {
