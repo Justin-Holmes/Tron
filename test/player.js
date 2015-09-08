58b19897ed69ca42e374cf02b4970b08b442d6c8
margie <ly.margarett@gmail.com>
2015-09-08 10:01:27 -0600

    change key bindings in other player.js tests from strings to integers

--- test/player.js
+++ test/player.js
@@ -61,10 +61,10 @@ describe('player', function() {
 });
 
 const otherKeys = {
-  left:  "65",
-  up:    "87",
-  right: "68",
-  down:  "83"
+  left:  65,
+  up:    87,
+  right: 68,
+  down:  83
 };
 
 describe('player within game object', function() {
